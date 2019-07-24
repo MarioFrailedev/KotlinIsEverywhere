4095621450b2e20a7f1233cdae95a01486d45de5
Mario Fraile <mariofrailedev@gmail.com>
2019-07-24 16:30:14 -0500

    kotlin basics

---
+++ app/src/main/java/com/manidevs/kotliniseverywhere/DataTypes.kt
@@ -0,0 +1,25 @@
+package com.manidevs.kotliniseverywhere
+
+fun main (args: Array<String>){
+    var name: String = "Morty"
+    name = "Sanchez"
+    print(name)
+
+    var age: Int = 22
+    var myAge = 29
+    var newAge = 34
+    newAge = 39
+
+    print(age +myAge +newAge)
+
+    var gender: Char = 'M'
+    var isLive:Boolean = false
+    val marks: Float = 95.5F
+
+    var porcentage: Double = 23.6665
+
+    print("My name is $name and I secured $marks in math examn  ")
+
+
+
+}
